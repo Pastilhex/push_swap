ae538d720b32dee0bd846cd4714f64fcb4b81c8d
Pastilhex <ivodanielmarques@gmail.com>
2023-04-06 12:01:50 +0100

    Pc

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: pastilhex <pastilhex@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 18:40:10 by pastilhex         #+#    #+#             */
-/*   Updated: 2023/04/05 14:00:18 by pastilhex        ###   ########.fr       */
+/*   Updated: 2023/04/06 11:29:06 by pastilhex        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,25 @@ int	main(int argc, char** argv)
 {
 	int	n;
 	t_list	*node;
-	t_list	**header;
+	t_list	**header_a;
 
-	header = (t_list **)malloc(sizeof(t_list*));
+	header_a = (t_list **)malloc(sizeof(t_list*));
 	n = 1;
 	if (argc == 2)
 	{
 		node = newlst(ft_atoi(argv[n]));
-		*header = node;
+		*header_a = node;
 	}
 	else if (argc > 2)
 	{
 		node = newlst(ft_atoi(argv[n++]));
-		*header = node;
+		*header_a = node;
 		while (n < argc)
-			add_back_lst(header, ft_atoi(argv[n++]));
+			add_back_lst(header_a, ft_atoi(argv[n++]));
 	}
 	else
 		return (0);
-	print_list(*header);
+	print_list(*header_a);
 
 	return (0);
 }
--- sa.c
+++ sa.c
@@ -6,17 +6,15 @@
 /*   By: pastilhex <pastilhex@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 13:11:38 by pastilhex         #+#    #+#             */
-/*   Updated: 2023/04/04 13:11:57 by pastilhex        ###   ########.fr       */
+/*   Updated: 2023/04/06 12:01:39 by pastilhex        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sa(t_list* list)
+void	sa(t_list **header_a, t_list *list)
 {
-	int	tmp;
-
-	tmp = list->value;
-	list->value = list->next->value;
-	list->next->value = tmp;
+	*header_a = list->next;
+	list->next = list->next->next;
+	list->next->next = *header_a;
 }
