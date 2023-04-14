2a549fd60f6d14f25e9d4a0c4a61d4fedd13bbbb
Ivo Daniel Alves Marques <ialves-m@c1r10s14.42porto.com>
2023-04-14 18:12:57 +0100

    42

--- brain.c
+++ brain.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   brain.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pastilhex <pastilhex@student.42.fr>        +#+  +:+       +#+        */
+/*   By: ialves-m <ialves-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 21:33:28 by ialves-m          #+#    #+#             */
-/*   Updated: 2023/04/13 21:54:46 by pastilhex        ###   ########.fr       */
+/*   Updated: 2023/04/14 15:12:00 by ialves-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ int	verify_order(t_list **header_a)
 	return (1);
 }
 
-//verifica o estado da lista a e da lista b
 void	begin(t_list **header_a, t_list **header_b)
 {
 	t_sort sort;
@@ -38,7 +37,7 @@ void	begin(t_list **header_a, t_list **header_b)
 	sort.last_value = find_last_value(*header_a);
 	while (1)
 	{
-		//pp(header_a, header_b);
+		// pp(header_a, header_b);
 		// usleep(500000);
 		if (verify_order(header_a) == 1 && size_list(header_b) != 0)
 			sorted_filled(&sort, header_a, header_b);
@@ -61,14 +60,14 @@ void	sorted_filled(t_sort *sort, t_list **header_a, t_list **header_b)
 	if (list_b->next == NULL)
 		{
 			if (list_b->value < list_a->value)
-				pb(header_a, header_b);
+				pa(header_a, header_b);
 			else
 				ra(sort, header_a, 0);
 		}
 	else if (list_b->value < list_b->next->value)
 		sb(header_b, 0);
 	else if (list_b->value > list_b->next->value && list_b->value < list_a->value)
-		pb(header_a, header_b);
+		pa(header_a, header_b);
 	else
 		ra(sort, header_a, 0);
 }
@@ -78,11 +77,7 @@ void	sorted_empty(t_sort *sort, t_list **header_a, t_list **header_b)
 	(void) sort;
 	(void) header_b;
 	if (verify_order(header_a) == 1)
-	{
-		// print_list(*header_a);
-		// print_list(*header_b);
 		exit (0);
-	}
 }
 
 void	unsorted_filled(t_sort *sort, t_list **header_a, t_list **header_b)
@@ -94,17 +89,17 @@ void	unsorted_filled(t_sort *sort, t_list **header_a, t_list **header_b)
 	list_b = *header_b;
 	(void) list_b;
 	if (list_b->value < list_a->value && list_b->value > sort->last_value)
-		pb(header_a, header_b);
+		pa(header_a, header_b);
 	// A < B && A > C
 	else if (list_a->value < list_a->next->value && list_a->value > sort->last_value)
 		ra(sort, header_a, 0);
 	// A < B && A < C
 	else if (list_a->value < list_a->next->value && list_a->value < sort->last_value)
-		pa(header_a, header_b);	
+		pb(header_a, header_b);	
 	// A > B == BIG && B == SMALL
 	else if (list_a->value > list_a->next->value && list_a->value == sort->biggest && list_a->next->value == sort->smallest && size_list(header_a) > 2)
 		ra(sort, header_a, 0);
-	// A > B
+	// A > B 
 	else if (list_a->value > list_a->next->value)
 		sa(header_a, 0);
 }
@@ -122,7 +117,7 @@ void	unsorted_empty(t_sort *sort, t_list **header_a, t_list **header_b)
 		ra(sort, header_a, 0);
 	// A < B && A < C
 	else if (list_a->value < list_a->next->value && list_a->value < sort->last_value)
-		pa(header_a, header_b);	
+		pb(header_a, header_b);	
 	// A > B == BIG && B == SMALL
 	else if (list_a->value > list_a->next->value && list_a->value == sort->biggest && list_a->next->value == sort->smallest && size_list(header_a) > 2)
 		ra(sort, header_a, 0);
--- pb.c
+++ pb.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pb.c                                               :+:      :+:    :+:   */
+/*   pa.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pastilhex <pastilhex@student.42.fr>        +#+  +:+       +#+        */
+/*   By: ialves-m <ialves-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/10 17:42:00 by pastilhex         #+#    #+#             */
-/*   Updated: 2023/04/13 20:53:04 by pastilhex        ###   ########.fr       */
+/*   Created: 2023/04/06 15:47:29 by ialves-m          #+#    #+#             */
+/*   Updated: 2023/04/14 14:31:42 by ialves-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ void	pb(t_list **header_a, t_list **header_b)
 	t_list *list_a;
 	t_list *list_b;
 
-	if (size_list(header_b) >= 1)
+	if (size_list(header_a) >= 1)
 	{
 		printf("pb\n");
 		list_a = *header_a;
 		list_b = *header_b;
 		tmp = (t_list *)malloc(sizeof(t_list));
-		tmp = new_list(list_b->value);
-		tmp->next = list_a;
-		*header_a = tmp;
-		*header_b = list_b->next;
+		tmp = new_list(list_a->value);
+		tmp->next = list_b;
+		*header_b = tmp;
+		*header_a = list_a->next;
 	}
 }
