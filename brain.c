8e51c645e9eeccb981ddc1f838c6a987de2df926
Pastilhex <ivodanielmarques@gmail.com>
2023-04-16 10:14:32 +0100

    Yoga

--- brain.c
+++ brain.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   brain.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pastilhex <pastilhex@student.42.fr>        +#+  +:+       +#+        */
+/*   By: ialves-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 21:33:28 by ialves-m          #+#    #+#             */
-/*   Updated: 2023/04/15 22:53:50 by pastilhex        ###   ########.fr       */
+/*   Updated: 2023/04/15 23:49:06 by ialves-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ void	begin(t_list **header_a, t_list **header_b)
 	sort.last_value = find_last_value(*header_a);
 	while (1)
 	{
-		pp(header_a, header_b);
-		printf("\n");
-		usleep(500000);
+		// pp(header_a, header_b);
+		// printf("\n");
+		// usleep(500000);
 		if (verify_order(header_a) == 0 && size_list(header_b) != 0)
 			unsorted_filled(&sort, header_a, header_b);
 
@@ -49,7 +49,7 @@ void	unsorted_empty(t_sort *sort, t_list **header_a, t_list **header_b)
 	find_smallest(sort, *header_a);
 	find_biggest(sort, *header_a);
 
-	printf("unsorted_empty\n");
+	//printf("unsorted_empty\n");
 	if (verify_in_order(sort, header_a) == 1 && size_list(header_b) == 0)
 		rra(sort, header_a, 0);
 
@@ -86,27 +86,27 @@ void	unsorted_filled(t_sort *sort, t_list **header_a, t_list **header_b)
 
 	list_a = *header_a;
 	list_b = *header_b;
-	(void) list_b;
+	find_biggest(sort, *header_b);
+	//printf("unsorted_filled\n");
 
-	printf("unsorted_filled\n");
 
-	if (list_b->value < list_a->value && list_b->value > sort->last_value)
-		pa(header_a, header_b);
-
-	else if (size_list(header_b) > 1 && (!verify_order(header_b) || !verify_in_order(sort, header_b)))
+	if (size_list(header_b) > 1 && (verify_rev_order(header_b)))
 	{
+		// if (list_b->value == sort->biggest && list_b->value < list_a->value)
+		// 	pa(header_a, header_b);
 		// A > B && A > C && B > C
 		if (list_b->value > list_b->next->value && list_b->value > sort->last_value_b && list_b->next->value > sort->last_value_b)
-			rb(sort, header_b, 0);
+			sb(header_b, 0);
+			//rb(sort, header_b, 0);
+		// A > B && A > C && B < C
+		else if (list_b->value > list_b->next->value && list_b->value > sort->last_value_b && list_b->next->value < sort->last_value_b)
+			sb(header_b, 0);
 		// A > B && A < C && B < C
 		else if (list_b->value > list_b->next->value && list_b->value < sort->last_value_b && list_b->next->value < sort->last_value_b)
 			rb(sort, header_b, 0);
 		// A < B && A < C && B > C
 		else if (list_b->value < list_b->next->value && list_b->value < sort->last_value_b && list_b->next->value > sort->last_value_b)
 			rb(sort, header_b, 0);
-		// A > B && A > C && B < C
-		else if (list_b->value > list_b->next->value && list_b->value > sort->last_value_b && list_b->next->value < sort->last_value_b)
-			sb(header_b, 0);
 		// A < B && A > C && B > C
 		else if (list_b->value < list_b->next->value && list_b->value > sort->last_value_b && list_b->next->value > sort->last_value_b)
 			rb(sort, header_b, 0);
@@ -114,6 +114,8 @@ void	unsorted_filled(t_sort *sort, t_list **header_a, t_list **header_b)
 		else if (list_b->value < list_b->next->value && list_b->value > sort->last_value_b && list_b->next->value > sort->last_value_b)
 			sb(header_b, 0);
 	}
+	else if (list_b->value < list_a->value && list_b->value > sort->last_value)
+		pa(header_a, header_b);
 	// A > B && A > C && B > C
 	else if (list_a->value > list_a->next->value && list_a->value > sort->last_value && list_a->next->value > sort->last_value)
 		sa(header_a, 0);
@@ -150,7 +152,7 @@ void	sorted_filled(t_sort *sort, t_list **header_a, t_list **header_b)
 	sort->last_value_b = find_last_value(*header_b);
 	find_biggest(sort, *header_b);
 
-	printf("sorted_filled\n");
+	//printf("sorted_filled\n");
 
 	if (list_b->value < list_a->value && list_b->value > sort->last_value)
 		pa(header_a, header_b);
@@ -176,7 +178,7 @@ void	sorted_empty(t_sort *sort, t_list **header_a, t_list **header_b)
 	(void) sort;
 	(void) header_b;
 
-	printf("sorted_empty\n");
+	//printf("sorted_empty\n");
 
 	if (verify_order(header_a) == 1)
 		exit (0);
