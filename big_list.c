81c2a678116a7b6d68e958b3031a765203692ebd
Pastilhex <ivodanielmarques@gmail.com>
2023-04-29 13:02:36 +0100

    PC

--- big_list.c
+++ big_list.c
@@ -6,7 +6,7 @@
 /*   By: ialves-m <ialves-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 17:32:42 by ialves-m          #+#    #+#             */
-/*   Updated: 2023/04/26 21:59:47 by ialves-m         ###   ########.fr       */
+/*   Updated: 2023/04/29 12:24:55 by ialves-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void	big_list(t_sort *s, t_list **header_a, t_list **header_b)
 			pp(header_a, header_b);
 		s->full_size++;
 		if (s->print)
-			usleep(500000);
+			usleep(250000);
 	}
 }
 
@@ -51,15 +51,13 @@ void	big_unsorted(t_sort *s, t_list **ha, t_list **hb)
 	find_smallest(s, *ha);
 	find_biggest(s, *ha);
 
-
-	//if (s->print)
-		//printf("Small Sort:%d\n", s->small_sort);
 	if (s->start_point == 0)
 	{
 		s->xfactor++;
 		s->start_point = size_list(ha);
 	}
-
+	if (s->print)
+		printf("START\n");
 	if (s->big_flag == 0)
 	{
 		while (size_list(hb) < s->xfactor * 3 && s->start_point >= 0)
@@ -70,14 +68,20 @@ void	big_unsorted(t_sort *s, t_list **ha, t_list **hb)
 		s->big_flag = 1;
 		s->count *= 2;
 		if (s->print)
-			printf("Count:%d\n", s->count);
+			printf("\nSTART: Count:%d\n", s->count);
 	}
 	else if (size_list(hb) != 0)
 	{
 		if (size_list(hb) != 0 && !verify_order(hb) && s->xfactor == 1)
-			sort_three_b(s, ha, hb);
-		else if (!(a->value < a->next->value && a->next->value < a->next->next->value) && s->small_sort == 0)
 		{
+			if (s->print)
+				printf("Sort b\n");	
+			sort_three_b(s, ha, hb);
+		}
+		else if (!(a->value < a->next->value && a->next->value < a->next->next->value) && s->small_sort == 0  && s->xfactor == 1)
+		{
+			if (s->print)
+				printf("Sort a\n");	
 			sort_three_a(s, ha, hb);
 			s->small_sort = 1;
 		}
@@ -90,7 +94,7 @@ void	big_unsorted(t_sort *s, t_list **ha, t_list **hb)
 			s->start_point--;
 			s->count--;
 			if (s->print)
-				printf("s.count:%d\n", s->count);
+				printf("1.count:%d\n", s->count);
 		}
 		else if (size_list(hb) != 0 && a->value < b->value && s->count - size_list(hb) > 0)
 		{
@@ -98,7 +102,7 @@ void	big_unsorted(t_sort *s, t_list **ha, t_list **hb)
 			s->start_point--;
 			s->count--;
 			if (s->print)
-				printf("s.count:%d\n", s->count);
+				printf("2.count:%d\n", s->count);
 		}
 
 		else if (size_list(hb) != 0 && s->count - size_list(hb) >= 0)
@@ -108,7 +112,7 @@ void	big_unsorted(t_sort *s, t_list **ha, t_list **hb)
 			s->start_point--;
 			s->count--;
 			if (s->print)
-				printf("s.count:%d\n", s->count);
+				printf("3.count:%d\n", s->count);
 		}
 	
 		else if (size_list(hb) == 0 && s->count > 0)
@@ -117,29 +121,30 @@ void	big_unsorted(t_sort *s, t_list **ha, t_list **hb)
 			s->start_point--;
 			s->count--;
 			if (s->print)
-				printf("s.count:%d\n", s->count);	
+				printf("4.count:%d\n", s->count);	
 		}
 		
 	}
-	else if (s->count >= 0)
+	else if (s->count > 0)
 	{
 		ra(s, ha, 0);
 		s->start_point--;
 		s->count--;
 		if (s->print)
-			printf("s.count:%d\n", s->count);	
+			printf("5.count:%d\n", s->count);	
 		
 	}
 	else if (size_list(hb) == 0 && s->count < 0)
 		s->big_flag = 0;
 
-	if (s->count < 0)
+	if (s->count <= 0)
 	{
 		s->big_flag = 0;
 		s->count = 0;
 		s->small_sort = 0;
 	}
-
+	if (s->print)
+		printf("start-point:%d\n", s->start_point);
 }
 
 void	sort_three_a(t_sort *s, t_list **ha, t_list **hb)
@@ -163,7 +168,7 @@ void	sort_three_a(t_sort *s, t_list **ha, t_list **hb)
 		{
 			sa(ha, 0);
 		}
-		else if (a->value < a->next->value && a->value < a->next->next->value && a->next->value > a->next->next->value)
+		else if ((a->value < a->next->value && a->value < a->next->next->value && a->next->value > a->next->next->value) || (a->value < a->next->value && a->value > a->next->next->value && a->next->value > a->next->next->value))
 		{
 			ra(s, ha, 0);
 			sa(ha, 0);
